9707fff8ae61d2e4d54d04a85fc3100462c86ca7
Anish Munimadugu <anish@Mac.grandenetworks.net>
2025-03-29 17:52:26 -0500

    added operand r16 instructions

--- CPU.cpp
+++ CPU.cpp
@@ -22,28 +22,43 @@ public:
     }
     // FIXME: add a destructor
 
+    void print_regs() {
+        printf("    |AF: 0x%0x    BC: 0x%0x    DE: 0x%0x    HL: 0x%0x    SP: 0x%0x    PC: 0x%0x|\n",
+                            AF,         BC,           DE,          HL,          SP,          PC);
+    }
+
 
     void execute(uint8_t cmd) {
-        uint16_t *reg_ptr;
-        // switch (cmd) {
-        //     case 0:     // nop
-        //         std::cout << "..nop. Breaking" << std::endl;
-        //         break;
-        //     case 0b00000011:
-        //         std::cout << "..matched binary lit. Breaking" << std::endl;
-        //         break;
-        // }
+
         if (cmd == 0) {
-            std::cout << "  ..nop. Breaking" << std::endl;
-        } else if ((cmd & 0b11) && ((cmd | 0b00110011) == 0b00110011)) {
-            std::cout << "  detected inc r16" << std::endl;
-            int reg_idx = (cmd & 0b00110000);
-            reg_ptr = get_r16(reg_idx);
+            std::cout << "  detected: nop" << std::endl;
+        } else if (match(cmd, "00xx0011")) {
+            // INC R16
+            printf("    detected: inc r16\n");
+            uint16_t *reg_ptr;
+            
+            reg_ptr = get_r16(((cmd >> 4) & 0b11));
             (*reg_ptr)++;
-            std::cout << "  reg_ptr index = " << reg_idx << "; value = " <<  static_cast<int>(*reg_ptr) << std::endl;
-        } else {
+        } else if (match(cmd, "00xx1011")) {
+            // DEC R16
+            printf("    detected: dec r16\n");
+            uint16_t *reg_ptr;
+            reg_ptr = get_r16(((cmd >> 4) & 0b11));
+            (*reg_ptr)--;
+        } else if (match(cmd, "00xx1001")) {
+            // add hl, r16
+            printf("    detected: add hl, r16\n");
+            uint16_t *reg_ptr;
+            reg_ptr = get_r16(((cmd >> 4) & 0b11));
+            HL += (*reg_ptr);
+
+        } 
+        
+        else {
             std::cout << "  default case" << std::endl;
         }
+
+        print_regs();
     }
 
 
@@ -51,6 +66,18 @@ public:
     //     std::cout << "Value of PC: " << PC << std::endl;
     // }
 private:
+    // Implement wildcard matching - cmd=1010 matches compare="10xx"
+    bool match(uint8_t cmd, const char *compare) {
+        int cmd_bit, compare_bit;
+        for (int i = 0; i < 8; i++) {
+            if (compare[i] == 'x') continue;
+            compare_bit = (int)compare[i] - 48; // convert char to int with ASCII value of 0
+            cmd_bit = (cmd >> (7-i)) & 1;   // get (7-i)-th bit of cmd
+
+            if (cmd_bit != compare_bit) return false;
+        }
+        return true;
+    }
     // Return address of the register (should remain static througout program runtime)
     uint16_t *get_r16(int index) {
         uint16_t *reg_ptr;
@@ -68,7 +95,7 @@ private:
                 reg_ptr = &SP;
                 break;
             default:
-                std::cerr << "Unexpected index to get_r16" << std::endl;
+                printf ("   Unexpected index to get_r16: %d\n", index);
         }
         // std::cout << "  Returning reg_ptr: " << reg_ptr << std::endl;
         return reg_ptr;
--- main.cpp
+++ main.cpp
@@ -2,15 +2,13 @@
 #include <fstream>
 
 #include <cstdint>
+// #include <cstdlib>
+#include <cstdio>
 #include "CPU.cpp"
 
 // emulate the cpu for the gameboy
 int main() {
-    std::cout << "hello" << std::endl;
-
     CPU *cpu = new CPU();
-
-
 
     std::ifstream inputFile("test-roms/prog_OR.gb", std::ios::binary); // Open the file
     if (!inputFile) {
@@ -21,7 +19,8 @@ int main() {
     char cmd;
     // main loop
     while (inputFile.read(&cmd, sizeof(cmd))) {
-        std::cout << "PC=" << cpu->PC << ": cmd=" << static_cast<int>(cmd) << std::endl;
+        // std::cout << "PC=" << cpu->PC << ": cmd=" << static_cast<int>(cmd) << std::endl;
+        printf ("PC=%d: cmd=0x%0x\n", cpu->PC, static_cast<int>(cmd));
 
         cpu->execute(static_cast<uint8_t>(cmd));
 
