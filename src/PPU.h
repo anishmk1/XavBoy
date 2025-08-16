0419a34ba55e41ce54eb446ffc4b0fda2b670e7d
Anish Munimadugu <anish@Mac.grandenetworks.net>
2025-08-16 18:49:13 -0500

    Created an SDL2 window

---
+++ src/PPU.h
@@ -0,0 +1,21 @@
+#define FIFO_DEPTH 16
+
+typedef struct {
+    int color;              //  a value between 0 and 3
+    int palette;            // on CGB a value between 0 and 7 and on DMG this only applies to objects
+    int sprite_priority;    // on CGB this is the OAM index for the object and on DMG this doesn’t exist
+    int bg_priotiy;         // holds the value of the OBJ-to-BG Priority bit
+} Pixel;
+
+class FIFO {
+public:
+    Pixel pixels[FIFO_DEPTH];
+
+    void push(int num);
+    int pop();
+};
+
+class PPU {
+public:
+    int mode;   // 0 - HBLANK; 1 - VBLANK; 2 - OAM SCAN; 3 - DRAW PIXELS
+};
